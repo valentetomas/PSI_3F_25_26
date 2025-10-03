2155d491709c9a303eda6038b65eb56f9da34aae
Tomás Valente <thomaz29valente@gmail.com>
2025-10-03 15:50:02 +0100

    exercício 1 - este é o meu primeiro commit

---
+++ M17C-Ex01/M17C-Ex01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M17C_Ex01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eu sou o Tomás Valente.");
+        }
+    }
+}
